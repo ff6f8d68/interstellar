31af5842aee737314019aed4cd2c9a7e58f3c103
ff6f8d68 <atom20003113@Mac-mini-von-atomdev.local>
2025-11-11 13:43:52 +0100

    fixed the java compile issue

--- src/main/java/team/nextlevelmodding/ar2/ModBlocks.java
+++ src/main/java/team/nextlevelmodding/ar2/ModBlocks.java
@@ -9,7 +9,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import mods.hexagonal.ar2.blocks.*;
+import team.nextlevelmodding.ar2.blocks.*;
 import team.nextlevelmodding.ar2.blocks.*;
 import team.nextlevelmodding.ar2.fluids.ModFluids;
 
